0711df8b128d3f4b8d8ddbad6c7fc2ac14ca3c5f
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-04-03 10:21:58 -0500

    added letter controller, recipients controller

--- app/controllers/api/letters_controller.rb
+++ app/controllers/api/letters_controller.rb
@@ -1,4 +1,60 @@
 class Api::LettersController < ApplicationController
 
+  before_action :authenticate_user
 
+  def index 
+    @letters = Letter.all
+
+    @letters = @letters.order(id: :asc)
+
+    render 'index.json.jb'
+
+  end 
+
+  def create
+    @letter = Letter.new(
+                          user_id: params[:user_id],
+                          referee_id: params[:referee_id],
+                          title: params[:title],
+                          recipient_info: params[:recipient_info],
+                          date: params[:date],
+                          section_background: params[:section_background],
+                          section_relationship: params[:section_relationship],
+                          section_strengths: params[:section_strengths],
+                          section_closer: params[:section_closer]
+                          )
+    if @letter.save
+      render "show.json.jb"
+    else
+      render json: {errors: @letter.errors.messages}, status: :unprocessable_entity
+    end 
+  end 
+
+  def show
+    @letter = Letter.find(params[:id])
+    render 'show.json.jb'
+  end 
+
+  def update
+    @letter = Letter.find(params[:id])
+
+    @letter.user_id = params[:user_id] || @letter.user_id
+    @letter.referee_id = params[:referee_id] || @letter.referee_id
+    @letter.title = params[:title] || @letter.title
+    @letter.recipient_info = params[:recipient_info] || @letter.recipient_info
+    @letter.date = params[:date] || @letter.date
+    @letter.section_background = params[:section_background] || @letter.section_background
+    @letter.section_relationship = params[:section_relationship] || @letter.section_relationship
+    @letter.section_strengths = params[:section_strengths] || @letter.section_strengths
+    @letter.section_closer = params[:section_closer] || section_closer
+
+    @letter.save
+    render 'show.json.jb'
+  end 
+
+  def destroy
+    letter = Letter.find(params[:id])
+    letter.destroy
+    render json: {message: "Letter successfully destroy"}
+  end 
 end
--- app/controllers/api/recipients_controller.rb
+++ app/controllers/api/recipients_controller.rb
@@ -1,2 +1,62 @@
 class Api::RecipientsController < ApplicationController
+
+  before_action :authenticate_user
+
+  def index
+    @recipients = Recipient.all
+
+    @recipients = @recipients.order(id: :asc)
+
+    render 'index.json.jb'
+
+  end 
+
+  def create
+    @recipient = Recipient.new(
+                                first_name: params[:first_name],
+                                last_name: params[:last_name],
+                                title: params[:title],
+                                company: params[:company],
+                                address: params[:address],
+                                city: params[:city],
+                                state: params[:state],
+                                zip: params[:zip],
+                                referee_id: params[:referee_id]
+                                )
+    if @recipient.save
+      render "show.json.jb"
+    else
+      render json: {errors: @recipients.errors.messages}, status: :unprocessable entity
+    end 
+
+    def show
+      @recipient = Recipient.find(params[:id])
+      render 'show.json.jb'
+    end 
+
+    def update
+      @recipient = Recipient.find(params[:id])
+
+      @recipient.
+
+
+
+
+
+
+
 end
+
+# create_table "recipients", force: :cascade do |t|
+#     t.string "first_name"
+#     t.string "last_name"
+#     t.string "title"
+#     t.string "company"
+#     t.string "address"
+#     t.string "city"
+#     t.string "state"
+#     t.string "zip"
+#     t.integer "referee_id"
+#     t.datetime "created_at", precision: 6, null: false
+#     t.datetime "updated_at", precision: 6, null: false
+#   end
