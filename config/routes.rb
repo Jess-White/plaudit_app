a3fe1025f0bd2878a58d7265a5f674cfba8f46e7
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-04-05 13:27:33 -0500

    added views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,30 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
+  namespace :api do
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+    post "/sessions" => "sessions#create"
+
+    get '/users' => 'users#index'
+    post '/users' => 'users#create'
+    get '/users/:id' => 'users#show'
+    patch '/users/:id' => 'users#update'
+    delete '/users/:id' => 'users#destroy'
+
+    get '/letters' => 'letters#index'
+    post '/letters' => 'letters#create'
+    get '/letters/:id' => 'letters#show'
+    patch '/letters/:id' => 'letters#update'
+    delete '/letters/:id' => 'letters#destroy'
+
+    get '/recipients' => 'recipients#index'
+    post '/recipients' => 'recipients#create'
+    get '/recipients/:id' => 'recipients#show'
+    patch '/recipients/:id' => 'recipients#update'
+    delete '/recipients/:id' => 'recipients#destroy'
+
+    get '/referees' => 'referees#index'
+    post '/referees' => 'referees#create'
+    get '/referees/:id' => 'referees#show'
+    patch '/referees/:id' => 'referees#update'
+    delete '/referees/:id' => 'referees#destroy'
 end
+end 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#letters
+Letter.create(user_id: 1, referee_id: 1, title: "Test Letter 1", recipient_info: "Test Letter 1", date: "2020-09-13", section_background: "Test Letter 1", section_relationship: "Test Letter 1", section_strengths: "Test Letter 1", section_closer: "Test Letter 1")
+Letter.create(user_id: 1, referee_id: 1, title: "Test Letter 2", recipient_info: "Test Letter 2", date: "2020-09-13", section_background: "Test Letter 2", section_relationship: "Test Letter 2", section_strengths: "Test Letter 2", section_closer: "Test Letter 2")
+Letter.create(user_id: 1, referee_id: 1, title: "Test Letter 3", recipient_info: "Test Letter 3", date: "2020-09-13", section_background: "Test Letter 3", section_relationship: "Test Letter 3", section_strengths: "Test Letter 3", section_closer: "Test Letter 3")
+Letter.create(user_id: 1, referee_id: 1, title: "Test Letter 4", recipient_info: "Test Letter 4", date: "2020-09-13", section_background: "Test Letter 4", section_relationship: "Test Letter 4", section_strengths: "Test Letter 4", section_closer: "Test Letter 4")
+Letter.create(user_id: 1, referee_id: 1, title: "Test Letter 5", recipient_info: "Test Letter 5", date: "2020-09-13", section_background: "Test Letter 5", section_relationship: "Test Letter 5", section_strengths: "Test Letter 5", section_closer: "Test Letter 5")
+Letter.create(user_id: 1, referee_id: 1, title: "Test Letter 6", recipient_info: "Test Letter 6", date: "2020-09-13", section_background: "Test Letter 6", section_relationship: "Test Letter 6", section_strengths: "Test Letter 6", section_closer: "Test Letter 6")
+
+#recipients
+Recipient.create(first_name: "Test Recipient 1", last_name: "Test Recipient 1", title: "", company: "", address: "", city: "", state: "", zip: "", referee_id: )
+Recipient.create(first_name: "", last_name: "", title: "", company: "", address: "", city: "", state: "", zip: "", referee_id: )
+Recipient.create(first_name: "", last_name: "", title: "", company: "", address: "", city: "", state: "", zip: "", referee_id: )
+Recipient.create(first_name: "", last_name: "", title: "", company: "", address: "", city: "", state: "", zip: "", referee_id: )
+Recipient.create(first_name: "", last_name: "", title: "", company: "", address: "", city: "", state: "", zip: "", referee_id: )
+Recipient.create(first_name: "", last_name: "", title: "", company: "", address: "", city: "", state: "", zip: "", referee_id: )
+
+# referees
+
+Referee.create(first_name: "", last_name: "", title: "", start_date: "", end_date: "", notes: "", email: "")
+Referee.create(first_name: "", last_name: "", title: "", start_date: "", end_date: "", notes: "", email: "")
+Referee.create(first_name: "", last_name: "", title: "", start_date: "", end_date: "", notes: "", email: "")
+Referee.create(first_name: "", last_name: "", title: "", start_date: "", end_date: "", notes: "", email: "")
+Referee.create(first_name: "", last_name: "", title: "", start_date: "", end_date: "", notes: "", email: "")
+Referee.create(first_name: "", last_name: "", title: "", start_date: "", end_date: "", notes: "", email: "")
+
+# users
+
+User.create(title: "", company_name: "", company_address: "", company_city: "", company_zip: "", email: "", background: "", first_name: "", last_name: "", password_digest: "")
+User.create(title: "", company_name: "", company_address: "", company_city: "", company_zip: "", email: "", background: "", first_name: "", last_name: "", password_digest: "")
+User.create(title: "", company_name: "", company_address: "", company_city: "", company_zip: "", email: "", background: "", first_name: "", last_name: "", password_digest: "")
+User.create(title: "", company_name: "", company_address: "", company_city: "", company_zip: "", email: "", background: "", first_name: "", last_name: "", password_digest: "")
+User.create(title: "", company_name: "", company_address: "", company_city: "", company_zip: "", email: "", background: "", first_name: "", last_name: "", password_digest: "")
+User.create(title: "", company_name: "", company_address: "", company_city: "", company_zip: "", email: "", background: "", first_name: "", last_name: "", password_digest: "")
